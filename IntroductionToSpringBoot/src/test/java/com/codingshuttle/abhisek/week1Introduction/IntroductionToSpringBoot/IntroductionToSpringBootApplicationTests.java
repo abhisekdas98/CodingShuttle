25e41a658a694381c95ebcb1b04e248f351874c5
Abhisek <abhisekdas98@gmail.com>
2024-07-01 20:48:08 +0530

    1.2 Setting UP Spring Boot application

---
+++ IntroductionToSpringBoot/src/test/java/com/codingshuttle/abhisek/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.abhisek.week1Introduction.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
