620e80410c3d22b4a1bd0496b94048d4dc02337d
Abhisek <abhisekdas98@gmail.com>
2024-07-09 21:21:24 +0530

    Spring Boot web development

---
+++ springbootwebtutuorial/src/main/java/com/example/springbootwebtutuorial/SpringBootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebtutuorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ springbootwebtutuorial/src/test/java/com/example/springbootwebtutuorial/SpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebtutuorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
