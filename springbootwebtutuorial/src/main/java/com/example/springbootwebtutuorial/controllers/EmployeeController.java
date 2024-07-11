160092d9cd44e6be123d74990dafa5070cb4b971
Abhisek <abhisekdas98@gmail.com>
2024-07-11 12:57:44 +0530

    Chapter 2.4 revised

--- springbootwebtutuorial/src/main/java/com/example/springbootwebtutuorial/controllers/EmployeeController.java
+++ springbootwebtutuorial/src/main/java/com/example/springbootwebtutuorial/controllers/EmployeeController.java
@@ -1,13 +1,9 @@
 package com.example.springbootwebtutuorial.controllers;
 
 import com.example.springbootwebtutuorial.dto.EmployeeDTO;
-import com.example.springbootwebtutuorial.entities.EmployeeEntity;
-import com.example.springbootwebtutuorial.reposiroties.EmployeeRepository;
 import com.example.springbootwebtutuorial.services.EmployeeService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
--- springbootwebtutuorial/src/main/java/com/example/springbootwebtutuorial/dto/EmployeeDTO.java
+++ springbootwebtutuorial/src/main/java/com/example/springbootwebtutuorial/dto/EmployeeDTO.java
@@ -1,7 +1,16 @@
 package com.example.springbootwebtutuorial.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmployeeDTO {
 
     Long id;
@@ -11,70 +20,4 @@ public class EmployeeDTO {
     Integer age;
     LocalDate dateOfJoining;
     Boolean isActive;
-
-    public EmployeeDTO(Long id, String name, String email, String address, Integer age, LocalDate dateOfJoining, Boolean isActive) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.address = address;
-        this.age = age;
-        this.dateOfJoining = dateOfJoining;
-        this.isActive = isActive;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public LocalDate getDateOfJoining() {
-        return dateOfJoining;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setDateOfJoining(LocalDate dateOfJoining) {
-        this.dateOfJoining = dateOfJoining;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
 }
